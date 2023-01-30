f0e8a6d7934e0a1e50cccb4ba4b184128f73f63d
OKAN <hacerozgen42@gmail.com>
2023-01-30 19:20:51 +0300

    Initial commit

---
+++ src/Gun01/Student.java
@@ -0,0 +1,13 @@
+package Gun01;
+
+public class Student {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba 1");
+
+        System.out.println("task 2");
+
+    }
+
+
+}
